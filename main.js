b372f1ecede6adff41790d6cd01e016236ff7755
Tetiana Anhelova <angelova.tatjana@gmail.com>
2020-03-16 20:27:09 +0100

    projecr js from 0

---
+++ main.js
@@ -0,0 +1,33 @@
+/* alert('Hi!'); */
+
+
+
+/* New */
+
+
+
+let name2 = "Mia";
+let isHappy = true;
+isHappy = false;
+/* let isHappy = "Tak" */
+isHappy = 3;
+
+const name = "Tania";
+const age = 32;
+/* name = "Ania"; */
+
+
+console.log('Hello!');
+console.log(isHappy); 
+console.log(`My name is ${name} and I'm ${age}`);
+
+if (name === name2){
+    console.log('There are two persons')
+}
+
+
+const today = Date.call();
+console.log(today);
+
+let arr = "[1,2,3]";
+console.log(typeof arr);
